01da40980d7aa2fb8056bb2e0b316b56a274ba4f
Waqas Ahmed <waqassoomroa@gmail.com>
2026-07-16 00:21:01 +0500

    Fixing STMP errors

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,5 +1,8 @@
 const nodemailer = require("nodemailer");
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
