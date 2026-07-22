4db6dd00f63d3309092ad9f9e55c955035d6cb81
Waqas Ahmed <waqassoomroa@gmail.com>
2026-07-22 22:24:05 +0500

    Changed database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
   } catch (error) {
